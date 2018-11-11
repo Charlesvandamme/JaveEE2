7c5448a4a17c4c77289f33d9cbeeb9f7e4383728
Nils Lefever <nilslef@gmail.com>
2018-11-11 16:22:45 +0100

    -m "Nils probeert zijn eerste commit"
    :wq
    
    :wq
    zz
    :wq

--- CarRental-ejb/src/java/session/ManagerSession.java
+++ CarRental-ejb/src/java/session/ManagerSession.java
@@ -14,6 +14,8 @@ import rental.CarRentalCompany;
 import rental.CarType;
 import rental.RentalStore;
 import rental.Reservation;
+import java.sql.*;
+import javax.persistence.Query;
 
 @Stateless
 public class ManagerSession implements ManagerSessionRemote {
@@ -28,15 +30,22 @@ public class ManagerSession implements ManagerSessionRemote {
     
     @Override
     public Set<CarType> getCarTypes(String company) {
-        try {
+        List<CarType> result;
+        /*try {
             return new HashSet<CarType>(RentalStore.getRental(company).getAllTypes());
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
             return null;
-        }
+        }*/
+        Query query = em.createQuery("SELECT DISTINCT car.type FROM Car car, CarRentalCompany company "
+                + "WHERE car MEMBER OF company.cars "
+                + "AND company.name = :companyName")
+                .setParameter("companyName", company);
+        result = query.getResultList();
+        return new HashSet<>(result);
     }
 
-    @Override
+ /*   @Override
     public Set<Integer> getCarIds(String company, String type) {
         Set<Integer> out = new HashSet<Integer>();
         try {
@@ -73,7 +82,7 @@ public class ManagerSession implements ManagerSessionRemote {
         }
         return out.size();
     }
-
+*/
     @Override
     public void addCarRentalCompany(CarRentalCompany crc) {
         List<Car> carsAtNewCompany = crc.getCars();
@@ -92,6 +101,68 @@ public class ManagerSession implements ManagerSessionRemote {
         } else {
             crc.addCar(car);
         }
+    }
+
+    @Override
+    public Set<String> getBestClients() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CarType getMostPopularCarTypeIn(String carRentalCompanyName, int year) throws Exception {
+        Query query = em.createQuery("SELECT carType, COUNT(carType) AS total FROM Reservation reservation, CarType carType "
+        + "WHERE reservation.rentalCompany = :companyName "
+            +"AND carType.companyName = :companyName "
+        + "GROUP BY carType "
+        + "ORDER BY total DESC")
+                .setParameter("companyName", carRentalCompanyName);
+        //TODO DATUM in verwerken!!!!
+        //TODO can zijn dat het List<CarType[]> moet zijn!
+        List<CarType> result = query.getResultList();
+        return (CarType) result.get(0);
+    }
+
+    @Override
+    public int getNumberOfReservationsForCarType(String company, String type) {
+        Query query = em.createQuery("SELECT reservation FROM Reservation reservation, Car car "
+        + "WHERE reservation.rentalCompany = :companyName "
+        + "AND car.type.name = :carTypeName "
+        + "AND reservation MEMBER OF car.reservations")
+                .setParameter("companyName", company)
+                .setParameter("carTypeName", type);
+        List<Reservation[]> result = query.getResultList();
+        return result.get(0).length;
+    }
+
+    @Override
+    public Set<Integer> getCarIds(String company, String type) {
+        Query query = em.createQuery("SELECT car.id FROM Car car, CarRentalCompany company "
+        + "WHERE company.name = :companyName "
+        + "AND car MEMBER OF company.cars"+ "AND car.type.name = :carTypeName")
+                .setParameter("companyName", company)
+                .setParameter("carTypeName", type);
+        List<Integer> result = query.getResultList();
+        return new HashSet<Integer>(result);
+    }
+
+    @Override
+    public int getNumberOfReservations(String company, String type, int carId) {
+        Query query = em.createQuery("SELECT reservation FROM Reservation reservation, CarRentalCompany company, Car car "
+        + "WHERE company.name = :companyName "
+        + "AND car.id = :carId "
+        + "AND reservation MEMBER OF car.reservations")
+                .setParameter("companyName", company)
+                .setParameter("carId", carId);
+        List<Reservation> result = query.getResultList();
+        return result.size();
+    }
+    
+    public int getNumberOfReservationsBy(String clientName) {
+        Query query = em.createQuery("SELECT reservation FROM Reservation reservation "
+        + "Where reservation.carRenter = :clientName")
+                .setParameter("clientName", clientName);
+        List<Reservation> result = query.getResultList();
+        return result.size();
     }
        
     
